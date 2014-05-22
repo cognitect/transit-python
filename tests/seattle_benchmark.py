b8022bcb34e2e883810ee624cfb46a3b5f4f54b7
Paul deGrandis <paul.degrandis@gmail.com>
2014-05-21 22:53:50 -0400

    Close the FD in the test

--- tests/seattle_benchmark.py
+++ tests/seattle_benchmark.py
@@ -9,7 +9,9 @@ def run_tests(data):
     JsonUnmarshaler().load(StringIO(data))
     print "Done: " + str((time.time() - t) * 1000.0)
 
-data = open("../transit/seattle-data0.tjs", 'r').read()
+fd = open("../transit/seattle-data0.tjs", 'r')
+data = fd.read()
+fd.close()
 
 for x in range(100):
     run_tests(data)
