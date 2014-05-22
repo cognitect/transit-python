5937fe810f454f9cdabd8c3c4f1380c1935386e2
Paul deGrandis <paul.degrandis@gmail.com>
2014-05-22 09:11:22 -0400

    Seattle should show introduced overhead

--- tests/seattle_benchmark.py
+++ tests/seattle_benchmark.py
@@ -2,12 +2,19 @@
 ## All rights reserved.
 
 from transit.reader import JsonUnmarshaler
+import json
 import time
 from StringIO import StringIO
 def run_tests(data):
+    datas = StringIO(data)
     t = time.time()
-    JsonUnmarshaler().load(StringIO(data))
-    print "Done: " + str((time.time() - t) * 1000.0)
+    JsonUnmarshaler().load(datas)
+    et = time.time()
+    datas = StringIO(data)
+    tt = time.time()
+    json.load(datas)
+    ett = time.time()
+    print "Done: " + str((et - t) * 1000.0) + "  --  raw JSON in: " + str((ett - tt) * 1000.0)
 
 fd = open("../transit/seattle-data0.tjs", 'r')
 data = fd.read()
@@ -15,5 +22,4 @@ fd.close()
 
 for x in range(100):
     run_tests(data)
-
 
