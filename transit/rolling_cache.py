4486aa2810f26ee4405d265e3a5ac2719ff5638b
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-06-27 14:18:56 -0600

    Added support for new cache codes.

--- transit/rolling_cache.py
+++ transit/rolling_cache.py
@@ -4,7 +4,7 @@
 import re
 
 FIRST_ORD = 33
-CACHE_SIZE = 94
+CACHE_SIZE = 94*94
 MIN_SIZE_CACHEABLE = 4
 ESCAPED = re.compile("^~(#|\$|:)")
 ## Copyright (c) Cognitect, Inc.
@@ -14,21 +14,32 @@ def is_cache_key(name):
     return len(name) > 0 and name[0] == "^"
 
 def encode_key(i):
-    return "^" + chr(i + FIRST_ORD)
+    lo = i % 94
+    hi = i // 94
+    if hi == 0:
+        return "^" + chr(i + FIRST_ORD)
+    else:
+        return "^" + chr(hi + FIRST_ORD) + chr(lo + FIRST_ORD)
 
 def decode_key(s):
-    return ord(s[1]) - FIRST_ORD
-
+    sz = len(s)
+    if sz == 2:
+        return ord(s[1]) - FIRST_ORD
+    else:
+        return (ord(s[2]) - FIRST_ORD) + (94 * (ord(s[1]) - FIRST_ORD))
 
 def is_cacheable(string, as_map_key=False):
     return string and len(string) >= MIN_SIZE_CACHEABLE and (as_map_key or ESCAPED.match(string))
 
 class RollingCache(object):
-
-
+    # (1) should we use list or dict on read side?
+    # (2) currently stores value read from the wire, should probably store value after decoding
+    # so we don't do multiple decodes. Sped up parsing in ruby by 30%.
     def __init__(self):
+        self.key_to_value = {}
         self.clear()
 
+    # if index rolls over... (bug)
     def decode(self, name, as_map_key=False):
         """Always returns the name"""
         if is_cache_key(name) and (name in self.key_to_value):
@@ -47,7 +58,6 @@ class RollingCache(object):
     def is_cache_full(self):
         return self.size() > CACHE_SIZE
 
-
     def encache(self, name):
         if self.is_cache_full():
             self.clear()
@@ -62,7 +72,8 @@ class RollingCache(object):
 
 
     def clear(self):
-        self.key_to_value = {}
+        # Flushing this one will introduce bugs.
+        # self.key_to_value = {}
         self.value_to_key = {}
 
 
