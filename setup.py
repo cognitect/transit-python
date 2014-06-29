ca8332a49102ea84b7c1d832a0d74fd11a970ccb
Paul deGrandis <paul.degrandis@gmail.com>
2014-06-29 12:32:46 -0400

    Prepare for roundtrip bin-verify

--- setup.py
+++ setup.py
@@ -2,11 +2,13 @@
 ## Copyright (c) Cognitect, Inc.
 ## All rights reserved.
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name="transit-python",
       version="0.1",
       description="Transit marshalling for Python",
       author="Cognitect",
       url="https://github.com/cognitect/transit-python",
-      packages=["distutils", "python-dateutil", "msgpack-python"])
+      packages=find_packages(),
+      install_requires=["python-dateutil", "msgpack-python"])
+
