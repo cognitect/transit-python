7ecb1ba2eee98186820f8561d0dfd06a74efdc08
Paul deGrandis <paul.degrandis@gmail.com>
2014-07-02 16:00:58 -0400

    The setup.py dist file should respect the revision versioning

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@
 ## All rights reserved.
 
 from setuptools import setup, find_packages
+import subprocess
+revision = subprocess.check_output("./bin/revision")
 
 setup(name="transit-python",
-      version="0.1",
+      version="0.0."+revision,
       description="Transit marshalling for Python",
       author="Cognitect",
       url="https://github.com/cognitect/transit-python",
