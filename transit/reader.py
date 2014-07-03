6ac77a65c1cc43e82b8c738e2f284ca60c13ca7d
Paul deGrandis <paul.degrandis@gmail.com>
2014-07-02 20:14:55 -0400

    There should be a way to establish a default IO source on the read side.

--- transit/reader.py
+++ transit/reader.py
@@ -7,13 +7,17 @@ from decoder import Decoder
 from collections import OrderedDict
 
 class Reader(object):
-    def __init__(self, protocol="json"):
+    def __init__(self, protocol="json", stream=None):
         if protocol in ("json", "json_verbose"):
             self.reader = JsonUnmarshaler()
         else:
             self.reader = MsgPackUnmarshaler()
+        self.default_stream = stream
 
-    def read(self, stream):
+    def read(self, stream=None):
+        stream = stream or self.default_stream
+        if stream is None:
+            raise Exception("Stream is None, please .read(some-stream)")
         return self.reader.load(stream)
 
     def register(self, key_or_tag, val):
