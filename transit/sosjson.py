91322a9632303dc01a6f35202c93b72c0de1fc8d
Ben Kamphaus <ben.kamphaus@gmail.com>
2014-09-05 21:04:31 -0600

    Doc on custom streamer.

--- transit/sosjson.py
+++ transit/sosjson.py
@@ -1,4 +1,7 @@
-# Ugly streamer
+# Simple object streaming in Python - just reads one complete JSON object
+# at a time and returns json.loads of that string.
+
+# Ugly implementation at moment
 from copy import copy
 import json
 
@@ -6,7 +9,8 @@ SKIP = [" ", "\n", "\t"]
 ESCAPE = "\\"
 
 def read_chunk(stream):
-    """Ignore whitespace outside of strings.
+    """Ignore whitespace outside of strings. If we hit a string, read it in
+    its entirety.
     """
     chunk = stream.read(1)
     while chunk in SKIP:
@@ -21,16 +25,23 @@ def read_chunk(stream):
     return chunk
 
 def items(stream):
+    """External facing items. Will return item from stream as available.
+    Currently waits in loop waiting for next item.
+    """
     for s in yield_json(stream):
         yield json.loads(s)
 
 def yield_json(stream):
+    """Uses array and object delimiter counts for balancing.
+    """
     buff = u""
     arr_count = 0
     obj_count = 0
     while True:
         buff += read_chunk(stream)
 
+        # If we finish parsing all objs or arrays, yield a finished JSON
+        # entity.
         if buff.endswith('{'):
             obj_count += 1
         if buff.endswith('['):
