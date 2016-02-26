1d1859632f251bd51ec4942f36fbe2f2baf2b6c8
Ben Kamphaus <ben.kamphaus@gmail.com>
2016-02-26 10:56:27 -0700

    keep generated setup.py stencil w/correct permissions

--- setup.py
+++ setup.py
@@ -16,10 +16,9 @@
 
 from setuptools import setup, find_packages
 import subprocess
-revision = subprocess.check_output("./bin/revision")
 
 setup(name="transit-python",
-      version="0.8."+revision,
+      version="0.8.279",
       description="Transit marshalling for Python",
       author="Cognitect",
       url="https://github.com/cognitect/transit-python",
