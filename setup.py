228a07779e34967906b9a0be4c0a1d41fdae81e6
Dan Dees <dand@appsmiths.com>
2016-08-07 21:28:24 +0530

    updated setup.py

--- setup.py
+++ setup.py
@@ -14,13 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from setuptools import setup, find_packages
+from distutils.core import setup
+from pkgutil import walk_packages
+
+import transit
+
+
+def find_packages(path, prefix=""):
+    yield prefix
+    prefix = prefix + "."
+    for _, name, ispkg in walk_packages(path, prefix):
+        if ispkg:
+            yield name
+
 
 setup(name="transit-python",
       version="0.8.279",
       description="Transit marshalling for Python",
       author="Cognitect",
       url="https://github.com/cognitect/transit-python",
-      packages=find_packages(),
-      install_requires=["python-dateutil", "msgpack-python"])
-
+      packages=list(find_packages(transit.__path__, transit.__name__)))
