6e658d325f3c69b45d196b2798ed0e96951d5cdb
Paul deGrandis <paul.degrandis@gmail.com>
2016-10-23 09:50:12 -0400

    Cleanup import; DRY

--- transit/read_handlers.py
+++ transit/read_handlers.py
@@ -12,8 +12,7 @@
 ## See the License for the specific language governing permissions and
 ## limitations under the License.
 
-from transit import pyversion
-from transit import transit_types
+from transit import pyversion, transit_types
 import uuid
 import ctypes
 import dateutil.parser
